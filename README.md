3b664d4383a3e8629be7eef278c2d1e946a540af
svenrdz <svenbmr@gmail.com>
2017-05-18 03:00:12 +0200

    Successful commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DFC-VAE
-Deep Feature Consistent Variational AutoEncoder (Pytorch)
+[Deep Feature Consistent Variational AutoEncoder] Implementation using Pytorch
+
+[Deep Feature Consistent Variational AutoEncoder]: https://arxiv.org/abs/1610.00291
--- main.py
+++ main.py
@@ -1,157 +1,327 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
+from __future__ import print_function
+import argparse
+import os
+import random
 import torch
-from torch import optim
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.models as models
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
 from torch.autograd import Variable
-from torch.utils.data import DataLoader
-from torchvision import transforms
-from torchvision.datasets import ImageFolder
-import matplotlib.pyplot as plt
-from time import time
-# from pycrayon import CrayonClient
 
-import model
+layer_names = ['conv1_1', 'relu1_1', 'conv1_2', 'relu1_2', 'pool1',
+               'conv2_1', 'relu2_1', 'conv2_2', 'relu2_2', 'pool2',
+               'conv3_1', 'relu3_1', 'conv3_2', 'relu3_2', 'conv3_3', 'relu3_3', 'conv3_4', 'relu3_4', 'pool3',
+               'conv4_1', 'relu4_1', 'conv4_2', 'relu4_2', 'conv4_3', 'relu4_3', 'conv4_4', 'relu4_4', 'pool4',
+               'conv5_1', 'relu5_1', 'conv5_2', 'relu5_2', 'conv5_3', 'relu5_3', 'conv5_4', 'relu5_4', 'pool5']
+default_content_layers = ['relu3_1', 'relu4_1', 'relu5_1']
 
-# Variables
-batchsize = 64
-imsize = 16
-cropsize = 16
-len_z = 500
-trainset = "/home/sven/Documents/Stage/Datasets/NUS-WIDE"
-testset = "/home/sven/Documents/Stage/Datasets/Landmarks"
-trainset = testset
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataroot', type=str, required=True,
+                    help='path to dataset folder (must follow PyTorch ImageFolder structure)')
+parser.add_argument('--workers', type=int,
+                    help='number of data loading workers, default=2', default=2)
+parser.add_argument('--batch_size', type=int,
+                    default=64, help='input batch size, default=64')
+parser.add_argument('--image_size', type=int, default=64,
+                    help='height/width length of the input images, default=64')
+parser.add_argument('--nz', type=int, default=100,
+                    help='size of the latent vector z, default=100')
+parser.add_argument('--nef', type=int, default=32,
+                    help='number of output channels for the first encoder layer, default=32')
+parser.add_argument('--ndf', type=int, default=32,
+                    help='number of output channels for the first decoder layer, default=32')
+parser.add_argument('--instance_norm', action='store_true',
+                    help='use instance norm layer instead of batch norm')
+parser.add_argument('--content_layers', type=str, nargs='?', default=None,
+                    help='name of the layers to be used to compute the feature perceptual loss')
+parser.add_argument('--niter', type=int, default=10,
+                    help='number of epochs to train for, default=10')
+parser.add_argument('--lr', type=float, default=0.0005,
+                    help='learning rate, default=0.0005')
+parser.add_argument('--beta1', type=float, default=0.5,
+                    help='beta1 for adam. default=0.5')
+parser.add_argument('--cuda', action='store_true', help='enables cuda')
+parser.add_argument('--ngpu', type=int, default=1,
+                    help='number of GPUs to use')
+parser.add_argument('--encoder', default='',
+                    help="path to encoder (to continue training)")
+parser.add_argument('--decoder', default='',
+                    help="path to decoder (to continue training)")
+parser.add_argument('--outf', default='./output',
+                    help='folder to output images and model checkpoints')
+parser.add_argument('--manualSeed', type=int, help='manual seed')
 
-# cc = CrayonClient('128.199.55.16')
-# exp = cc.open_experiment('DFCVAE')
 
-DFCVAE = model.DFCVAE(len_z, imsize).cuda()
-vgg_conv = model.VGG().cuda()
-descriptor_net = model.VGG(True).cuda()
+args = parser.parse_args()
+print(args)
 
-# Dataset
-def get_data(root=trainset):
-    """
-    Returns a preprocessed DataLoader from root folder
-    """
-    def rgb2bgr(img):
-        img = torch.np.asarray(img)
-        return img[:,:,::-1]
-    def sub_mean(img):
-        means_bgr = torch.Tensor([103.939, 116.779, 123.68]).unsqueeze(1).unsqueeze(2)
-        return img - means_bgr.expand_as(img)
-    custom_transform = transforms.Compose([
-                        transforms.Lambda(rgb2bgr),
-                        transforms.ToPILImage(),
-                        transforms.Scale(imsize),
-                        transforms.CenterCrop(cropsize),
-                        transforms.ToTensor(),
-                        transforms.Lambda(lambda x: x.mul(255)),
-                        transforms.Lambda(sub_mean)])
-    data = ImageFolder(root=root, transform=custom_transform)
-    return DataLoader(data, shuffle=True, batch_size=batchsize, num_workers=1)
+try:
+    os.makedirs(args.outf)
+except OSError:
+    pass
 
-def loss_func(recon, targets, mu, logvar):
-    for i, mse_layer in enumerate(descriptor_net.content):
-        mse_layer.target = targets[i]
-    _, content_losses = descriptor_net(recon)
-    FPL = sum(content_losses)
-    KLD_element = mu.pow(2).add_(logvar.exp()).mul_(-1).add_(1).add_(logvar)
-    KLD = torch.sum(KLD_element).mul(-0.5)
-    return FPL + KLD
+if args.manualSeed is None:
+    args.manualSeed = random.randint(1, 10000)
+print("Random Seed: ", args.manualSeed)
+random.seed(args.manualSeed)
+torch.manual_seed(args.manualSeed)
+if args.cuda:
+    torch.cuda.manual_seed_all(args.manualSeed)
 
-optimizer = optim.Adam(DFCVAE.parameters(), lr=5e-4, betas=(0.5, 0.999))
+cudnn.benchmark = True
 
-def train(epoch):
-    DFCVAE.train()
-    train_loss = 0
-    part_loss = 0
-    for batch_idx, (img,_) in enumerate(train_data):
-        img = Variable(img.cuda())
+if torch.cuda.is_available() and not args.cuda:
+    print("WARNING: You have a CUDA device, so you should probably run with --cuda")
 
+# Dataset loading
+# Normalization mean and standard deviation are set accordingly to the ones used
+# to train the vgg19 in torchvision model zoo
+# https://github.com/pytorch/vision
+transform = transforms.Compose([
+    transforms.Scale(args.image_size),
+    transforms.CenterCrop(args.image_size),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=(0.485, 0.456, 0.406),
+                         std=(0.229, 0.224, 0.225))])
+datafolder = dset.ImageFolder(root=args.dataroot, transform=transform)
+dataloader = torch.utils.data.DataLoader(
+    datafolder, shuffle=True, batch_size=args.batch_size, num_workers=args.workers, drop_last=True)
+
+ngpu = int(args.ngpu)
+nz = int(args.nz)
+nef = int(args.nef)
+ndf = int(args.ndf)
+nc = 3
+if args.instance_norm:
+    Normalize = nn.InstanceNorm2d
+else:
+    Normalize = nn.BatchNorm2d
+if args.content_layers is None:
+    content_layers = default_content_layers
+
+
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    if isinstance(m, nn.Conv2d):
+        init.kaiming_normal(m.weight.data, a=0.01)
+        m.bias.data.zero_()
+    elif isinstance(m, nn.BatchNorm2d):
+        init.normal(m.weight.data, std=0.015)
+        m.bias.data.zero_()
+
+
+class _VGG(nn.Module):
+
+    def __init__(self, ngpu):
+        super(_VGG, self).__init__()
+
+        self.ngpu = ngpu
+        features = models.vgg19(pretrained=True).features
+
+        self.features = nn.Sequential()
+        for i, module in enumerate(features):
+            name = layer_names[i]
+            self.features.add_module(name, module)
+
+    def forward(self, input):
+        batch_size = input.size(0)
+        all_outputs = []
+        output = input
+        for name, module in self.features.named_children():
+            if isinstance(output.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+                output = nn.parallel.data_parallel(
+                    module, output, range(self.ngpu))
+            else:
+                output = module(output)
+            if name in content_layers:
+                all_outputs.append(output.view(batch_size, -1))
+        return all_outputs
+
+
+descriptor = _VGG(ngpu)
+print(descriptor)
+
+
+class _Encoder(nn.Module):
+
+    def __init__(self, ngpu):
+        super(_Encoder, self).__init__()
+        self.ngpu = ngpu
+        self.encoder = nn.Sequential(
+            nn.Conv2d(nc, nef, 4, 2, padding=1),
+            nn.LeakyReLU(0.2, True),
+            Normalize(nef),
+
+            nn.Conv2d(nef, nef * 2, 4, 2, padding=1),
+            nn.LeakyReLU(0.2, True),
+            Normalize(nef * 2),
+
+            nn.Conv2d(nef * 2, nef * 4, 4, 2, padding=1),
+            nn.LeakyReLU(0.2, True),
+            Normalize(nef * 4),
+
+            nn.Conv2d(nef * 4, nef * 8, 4, 2, padding=1),
+            nn.LeakyReLU(0.2, True),
+            Normalize(nef * 8)
+        )
+        self.mean = nn.Linear(nef * 8 * 4 * 4, nz)
+        self.logvar = nn.Linear(nef * 8 * 4 * 4, nz)
+
+    def sampler(self, mean, logvar):
+        std = logvar.mul(0.5).exp_()
+        if args.cuda:
+            eps = torch.cuda.FloatTensor(std.size()).normal_()
+        else:
+            eps = torch.FloatTensor(std.size()).normal_()
+        eps = Variable(eps)
+        return eps.mul(std).add_(mean)
+
+    def forward(self, input):
+        batch_size = input.size(0)
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            hidden = nn.parallel.data_parallel(
+                self.encoder, input, range(self.ngpu))
+            hidden = hidden.view(batch_size, -1)
+            mean = nn.parallel.data_parallel(
+                self.mean, hidden, range(self.ngpu))
+            logvar = nn.parallel.data_parallel(
+                self.logvar, hidden, range(self.ngpu))
+        else:
+            hidden = self.encoder(input)
+            hidden = hidden.view(batch_size, -1)
+            mean, logvar = self.mean(hidden), self.logvar(hidden)
+        latent_z = self.sampler(mean, logvar)
+        return latent_z
+
+
+encoder = _Encoder(ngpu)
+encoder.apply(weights_init)
+if args.encoder != '':
+    encoer.load_state_dict(torch.load(args.encoder))
+print(encoder)
+
+
+class _Decoder(nn.Module):
+
+    def __init__(self, ngpu):
+        super(_Decoder, self).__init__()
+        self.ngpu = ngpu
+        self.decoder_dense = nn.Sequential(
+            nn.Linear(nz, ndf * 8 * 4 * 4),
+            nn.ReLU(True)
+        )
+        self.decoder_conv = nn.Sequential(
+            nn.UpsamplingNearest2d(scale_factor=2),
+            nn.Conv2d(ndf * 8, ndf * 4, 3, padding=1),
+            nn.LeakyReLU(0.2, True),
+            Normalize(ndf * 4, 1e-3),
+
+            nn.UpsamplingNearest2d(scale_factor=2),
+            nn.Conv2d(ndf * 4, ndf * 2, 3, padding=1),
+            nn.LeakyReLU(0.2, True),
+            Normalize(ndf * 2, 1e-3),
+
+            nn.UpsamplingNearest2d(scale_factor=2),
+            nn.Conv2d(ndf * 2, ndf, 3, padding=1),
+            nn.LeakyReLU(0.2, True),
+            Normalize(ndf, 1e-3),
+
+            nn.UpsamplingNearest2d(scale_factor=2),
+            nn.Conv2d(ndf, nc, 3, padding=1)
+        )
+
+    def forward(self, input):
+        batch_size = input.size(0)
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            hidden = nn.parallel.data_parallel(
+                self.decoder_dense, input, range(self.ngpu))
+            hidden = hidden.view(batch_size, ndf * 8, 4, 4)
+            output = nn.parallel.data_parallel(
+                self.decoder_conv, input, range(self.ngpu))
+        else:
+            hidden = self.decoder_dense(input).view(
+                batch_size, ndf * 8, 4, 4)
+            output = self.decoder_conv(hidden)
+        return output
+
+
+decoder = _Decoder(ngpu)
+decoder.apply(weights_init)
+if args.decoder != '':
+    decoder.load_state_dict(torch.load(args.decoder))
+print(decoder)
+
+
+mse = nn.MSELoss()
+def fpl_criterion(recon_features, targets):
+    fpl = 0
+    for f, target in zip(recon_features, targets):
+        fpl += mse(f, target.detach())#.div(f.size(1))
+    return fpl
+
+kld_criterion = nn.KLDivLoss()
+
+
+input = torch.FloatTensor(
+    args.batch_size, nc, args.image_size, args.image_size)
+latent_labels = torch.FloatTensor(args.batch_size, nz).fill_(1)
+
+if args.cuda:
+    encoder = encoder.cuda()
+    decoder = decoder.cuda()
+    descriptor = descriptor.cuda()
+    input = input.cuda()
+    latent_labels = latent_labels.cuda()
+
+input = Variable(input)
+latent_labels = Variable(latent_labels)
+
+# setup optimizer
+parameters = list(encoder.parameters()) + list(decoder.parameters())
+optimizer = optim.Adam(parameters, lr=args.lr, betas=(args.beta1, 0.999))
+
+encoder.train()
+decoder.train()
+
+train_loss = 0
+for epoch in range(args.niter):
+    for i, (batch, _) in enumerate(dataloader):
         optimizer.zero_grad()
+        input.data.copy_(batch)
 
-        targets,_ = vgg_conv(img.clone())
-        recon, mu, logvar,_ = DFCVAE(img)
+        latent_z = encoder(input)
+        targets = descriptor(input)
+        kld = kld_criterion(F.log_softmax(latent_z), latent_labels)
+        kld.backward(retain_variables=True)
 
-        loss = loss_func(recon, targets, mu, logvar)
-        loss.backward()
+        recon = decoder(latent_z)
+        recon_features = descriptor(recon)
+        fpl = fpl_criterion(recon_features, targets)
+        fpl.backward()
 
+        loss = kld + fpl
         train_loss += loss.data[0]
         optimizer.step()
+        print('[{}/{}][{}/{}] FPL: {:.4f} KLD: {:.4f}'.format(
+              epoch, args.niter, i, len(dataloader),
+              fpl.data[0], kld.data[0]))
+        if i % 100 == 0:
+            vutils.save_image(input.data,
+                              '{}/inputs.png'.format(args.outf),
+                              normalize=True)
+            vutils.save_image(recon.data,
+                              '{}/reconstructions_epoch_{:03d}.png'.format(
+                                  args.outf, epoch),
+                              normalize=True)
 
-        # Logging
-        if batch_idx % 5 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(img), len(train_data.dataset),
-                100. * batch_idx / len(train_data),
-                loss.data[0] / len(img)))#, end='\r', flush=True)
-            # exp.add_scalar_value('loss', loss.data[0] / len(img), wall_time=time(), step=epoch*len(train_data.dataset)/batchsize+batch_idx)
-    print('\n====> Epoch: {} Average loss: {:.4f}'.format(
-          epoch, train_loss / len(train_data.dataset)))
-    return img, recon, train_loss
-
-def test(epoch):
-    DFCVAE.eval()
-    test_loss = 0
-    for img,_ in test_data:
-        img = Variable(img.cuda(), volatile=True)
-
-        targets,_ = vgg_conv(img.clone())
-        recon, mu, logvar,_ = DFCVAE(img)
-
-        loss = loss_func(recon, targets, mu, logvar)
-        test_loss += loss.data[0]
-    test_loss /= len(test_data.dataset)
-    print('====> Test set loss: {:.4f}'.format(test_loss))
-    return test_loss
-
-def save(path, epoch, train_loss, test_loss):
-    torch.save({
-            'epoch': epoch + 1,
-            'state_dict': DFCVAE.state_dict(),
-            'train_loss': train_loss,
-            'test_loss': test_loss
-        }, path)
-
-def load(model, path):
-    if os.path.isfile(path):
-        print("=> loading checkpoint '{}'".format(args.resume))
-        checkpoint = torch.load(path)
-        epoch = checkpoint['epoch']
-        train_loss = checkpoint['train_loss']
-        test_loss = checkpoint['test_loss']
-        model.load_state_dict(checkpoint['state_dict'])
-        return epoch, train_loss, test_loss
-
-def imshow(data):
-    """
-    Unprocess and show the image
-    !!! Input is a tensor !!!
-    """
-    def bgr2rgb(img):
-        img = torch.np.asarray(img)
-        return img[:,:,::-1]
-    def add_mean(img):
-        means_bgr = torch.Tensor([103.939, 116.779, 123.68]).unsqueeze(1).unsqueeze(2)
-        return img + means_bgr.expand_as(img)
-    unloader = transforms.Compose([
-                transforms.Lambda(add_mean),
-                transforms.Lambda(lambda x: x.mul(1/255.)),
-                transforms.ToPILImage(),
-                transforms.Lambda(bgr2rgb),
-                transforms.ToPILImage()])
-    img = data.clone().cpu().resize_(3,cropsize,cropsize)
-    plt.imshow(unloader(img))
-    plt.show()
-
-train_data = get_data()
-test_data = get_data(testset)
-
-for epoch in range(10):
-    img, recon, train_loss = train(epoch+1)
-    imshow(img.data)
-    imshow(recon.data)
-    test_loss = test(epoch)
-    save('dfcvae_{}_{}'.format(epoch+1, time()), epoch+1, train_loss, test_loss)
-    optimizer.param_groups[0]['lr'] *= 0.5
+    # do checkpointing
+    torch.save(encoder.state_dict(), '{}/encoder_epoch_{}.pth' % (args.outf, epoch))
+    torch.save(decoder.state_dict(), '{}/decoder_epoch_{}.pth' % (args.outf, epoch))
